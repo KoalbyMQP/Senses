9bf755711ac03096d5c6a01a46ec6b8e7e225478
thanhan <76832709+annpt625@users.noreply.github.com>
2024-11-25 12:37:37 -0500

    Update run_monitor.sh

--- tools/perf/hw/rpi/resmon/v0-1/monitoring/run_monitor.sh
+++ tools/perf/hw/rpi/resmon/v0-1/monitoring/run_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PYTHONPATH="/home/finley/Documents/GitHub/RaspberryPi-Code_24-25/tools/perf/hw/rpi/resmon/v0-1:$PYTHONPATH"
+export PYTHONPATH="/home/finley/Documents/GitHub/Vision-Backup/tools/perf/hw/rpi/resmon/v0-1:$PYTHONPATH"
 python3 cli.py "$@"
