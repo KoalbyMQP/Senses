2d53bb4703c43369520688f690b1e8be9d3c00db
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-02 15:33:35 -0500

    Update voice_helper.sh

--- vdepthai/depthai/voice_helper.sh
+++ vdepthai/depthai/voice_helper.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-export PYTHONPATH="//home/finley/Documents/GitHub/Vision-Backup:$PYTHONPATH"
-python3 depthai_demo.py -cnn yolo-v3-tiny-tf -s color "$@"
+
+# Get the absolute path to Vision-Backup
+VISION_BACKUP_PATH="/home/finley/Documents/GitHub/Vision-Backup"
+
+# Change to Vision-Backup directory
+cd "$VISION_BACKUP_PATH"
+
+# Activate the virtual environment
+source "$VISION_BACKUP_PATH/vdepthai/bin/activate"
+
+# Set PYTHONPATH
+export PYTHONPATH="$VISION_BACKUP_PATH:$PYTHONPATH"
+
+# Run depthai_demo.py
+python3 vdepthai/depthai/depthai_demo.py -cnn yolo-v3-tiny-tf -s color "$@"
