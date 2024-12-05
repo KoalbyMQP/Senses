56bdb375ef62e474dcfdfd5eb549642e5076b3f1
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-05 15:16:32 -0500

    yolo v8 testing

--- vdepthai/depthai/voice_helper.sh
+++ vdepthai/depthai/voice_helper.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 export PYTHONPATH="/home/finley/Documents/GitHub/Vision-Backup:$PYTHONPATH"
 
-# Activate virtual environment if not already activated
-if [[ -z "${VIRTUAL_ENV}" ]]; then
-    source ./venv/bin/activate
-fi
-
 # Run the demo with YOLO model and color camera
-python3 depthai_demo.py -cnn yolo-v3-tiny-tf -s color "$@"
+python3 depthai_demo.py -cnn yolov8n_coco_640x352 -s color "$@"
