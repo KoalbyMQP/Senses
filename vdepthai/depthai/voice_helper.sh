a12c87cf17d6908175e2e2f5e71ab8b429f1325d
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-05 15:23:45 -0500

    yolo v8 config

--- vdepthai/depthai/voice_helper.sh
+++ vdepthai/depthai/voice_helper.sh
@@ -2,4 +2,4 @@
 export PYTHONPATH="/home/finley/Documents/GitHub/Vision-Backup:$PYTHONPATH"
 
 # Run the demo with YOLO model and color camera
-python3 depthai_demo.py -cnn yolov8n_coco_640x352 -s color "$@"
+python3 depthai_demo.py -cnn yolov8n_coco_640x352 -s color "$@"fps=30
