423e4a46e5ed59e083163eec47221c1b10004590
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-08 17:21:05 -0500

    Update depthai_demo.py

--- vdepthai/depthai/depthai_demo.py
+++ vdepthai/depthai/depthai_demo.py
@@ -1385,7 +1385,7 @@ class SpeechEnabledDemo(Demo):
             else:
                 activate_cmd = ""
             
-            robot_cmd = f"lxterminal -e 'bash -c \"{activate_cmd}python3 {robot_script_path} 2>/dev/null; echo Press Enter to close...; read\"'"
+            robot_cmd = f"lxterminal -e 'bash -c \"{activate_cmd}python3 {robot_script_path}; echo Press Enter to close...; read\"'"
             
             self.robot_process = subprocess.Popen(
                 robot_cmd,
