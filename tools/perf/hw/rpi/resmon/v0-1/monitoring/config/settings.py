5deb13e08e741952eaa744224235f4567260bf66
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-11 16:07:38 -0500

    Revert "clean up"
    This reverts commit 93fe1205fc58520301d15bbd1c15f2e660a4730b.

--- tools/perf/hw/rpi/resmon/v0-1/monitoring/config/settings.py
+++ tools/perf/hw/rpi/resmon/v0-1/monitoring/config/settings.py
@@ -8,7 +8,7 @@ class Settings:
     def __init__(self):
         self.settings = {
             'depthai_path': Path('/home/finley/Documents/GitHub/Vision-Backup/vdepthai/depthai'),
-            'venv_path': Path('/home/finley/Documents/GitHub/Vision-Backup/vdepthai/depthai/venv'),
+            'venv_path': Path('/home/finley/Documents/GitHub/Vision-Backup/vdepthai'),
             'sampling_interval': 0.1  # seconds
         }
 
