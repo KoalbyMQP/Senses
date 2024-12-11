4d48a00ea563b17c151ba7d1beec279105d95ff9
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-11 17:32:02 -0500

    Update voice_helper.sh

--- vdepthai/depthai/voice_helper.sh
+++ vdepthai/depthai/voice_helper.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 export PYTHONPATH="/home/finley/Documents/GitHub/Vision:$PYTHONPATH"
-
-
-
-python3 depthai_demo.py -cnn yolo-v3-tiny-tf -s color "$@"
+python3 depthai_demo.py -s color 
