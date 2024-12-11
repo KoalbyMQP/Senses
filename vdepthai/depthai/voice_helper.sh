040c352a0f390fafb6c646a11c506dbdf61951f6
thanhan <76832709+annpt625@users.noreply.github.com>
2024-12-11 15:52:59 -0500

    real robot

--- vdepthai/depthai/voice_helper.sh
+++ vdepthai/depthai/voice_helper.sh
@@ -2,5 +2,4 @@
 export PYTHONPATH="/home/finley/Documents/GitHub/Vision-Backup:$PYTHONPATH"
 python3 depthai_demo.py -cnn yolo-v3-tiny-tf -s color "$@" 
 
-
 #python3 depthai_demo.py -cnn yolov8n_coco_640x352 -s color "$@"
