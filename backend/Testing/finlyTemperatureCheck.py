e7b01b5398b07bf3c6a1202a9405422714fa1a29
KoalbyMQP <atphan@wpi.edu>
2025-03-06 15:57:27 -0500

    test

--- backend/Testing/finlyTemperatureCheck.py
+++ backend/Testing/finlyTemperatureCheck.py
@@ -122,7 +122,7 @@ while time.time() - simStartTime < 2:
     robot.moveAllToTarget()
 
 # Transform the coordinates from camera frame to robot frame
-B = np.array([[final_points[0]], [final_points[2]], [final_points[1]], [1]])
+B = np.array([[final_points[0]], [final_points[1]], [final_points[2]], [1]])
 A = camera_frame_transformation
 C = np.dot(A, B)
 print(f"Transformed forehead coordinates: {C}")
@@ -131,7 +131,7 @@ print(f"Transformed forehead coordinates: {C}")
 x_coord = float(C[0][0])
 y_coord = float(C[1][0])
 z_coord = float(C[2][0])
-
+    
 # Set up trajectory for moving to forehead
 leftArmTraj = [
     [[0,0,0], [20,20,20]],
