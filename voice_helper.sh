7df308526334837ad1fbd3246324004b1a59bb6d
KoalbyMQP <atphan@wpi.edu>
2025-03-25 17:45:25 -0400

    Revert "test"
    This reverts commit 58b9dfe9944f918446141bed200c67e80afaecb9.

--- voice_helper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PYTHONPATH="/home/finley/Documents/GitHub/Senses/Vision:$PYTHONPATH"
-
-# Run the demo with YOLO model and color camera
-python3 depthai_demo.py -cnn yolo-v3-tiny-tf -s color "$@" 
-#python3 depthai_demo.py -cnn yolov8n_coco_640x352 -s color "$@"
