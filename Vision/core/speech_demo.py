b4fa48b7933353d4286630cbeaca91beebef0859
KoalbyMQP <atphan@wpi.edu>
2025-04-18 18:22:26 -0400

    Update speech_demo.py

--- Vision/core/speech_demo.py
+++ Vision/core/speech_demo.py
@@ -19,7 +19,7 @@ class SpeechEnabledDemo(Demo):
         self.face_process = None
         self.current_target = None
         self.measurement_buffer = []
-        self.max_buffer_size = 200
+        self.max_buffer_size = 20000
         self.coordinates_sent = False 
         
         # Camera parameters for 1080p
