b1af5e2fb7b88a307b1fb744e6aa9c36195ac006
KoalbyMQP <atphan@wpi.edu>
2025-04-18 15:38:09 -0400

    Create finlySwapMove.py

---
+++ Gripper/swap/finlySwapMove.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ikpy.chain import Chain
+from ikpy.utils import plot as plot_utils
+import sys, time, math, array
+import numpy as np
+sys.path.append("./")
+from backend.KoalbyHumanoid.Robot import Robot
+from backend.KoalbyHumanoid.trajPlannerTime import TrajPlannerTime
+from backend.Testing import finlyViaPoints as via
+is_real = False
+robot = Robot(is_real)
+print("Setup Complete")
+robot.motors[25].target = (math.radians(0), 'P')
+robot.motors[26].target = (math.radians(0), 'P')
+robot.motors[27].target = (math.radians(0), 'P')
+robot.motors[5].target = (math.radians(0), 'P')
+robot.motors[6].target = (math.radians(0), 'P')
+robot.motors[7].target = (math.radians(0), 'P')
+robot.motors[8].target = (math.radians(0), 'P')
+robot.motors[9].target = (math.radians(0), 'P')
+robot.motors[10].target =(math.radians(0), 'P')
+# centering all angles to zero
+simStartTime = time.time()
+while time.time() - simStartTime < 2:
+    time.sleep(0.01)
+    robot.IMUBalance(0,0)
+    robot.moveAllToTarget()
+leftArmTraj = [
+    [[0,0,0,0,0],[20,20,20,20,20],[40,40,40,40,40],[60,60,60,60,60]],
+    [
+    [0,0,0,0,0],
+    [0,-90,0,0,0],
+    [-90,-90,0,0,0],
+    [-117,-103,0,-53,45]
+    ] ,
+    [[0,0,0,0,0], [1,1,1,1,1], [1,1,1,1,1], [0,0,0,0,0]],
+    [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
+]
+lArm_tj_joint = TrajPlannerTime(leftArmTraj[0], leftArmTraj[1], leftArmTraj[2], leftArmTraj[3])
+startTime = time.time()
+while time.time() - startTime < 60:
+    target_position_joint = lArm_tj_joint.getQuinticPositions(time.time() - startTime)
+    print(target_position_joint)
+    robot.motors[5].target = (math.radians(target_position_joint[0]), 'P')
+    robot.motors[6].target = (math.radians(target_position_joint[1]), 'P')
+    robot.motors[7].target = (math.radians(target_position_joint[2]), 'P')
+    robot.motors[8].target = (math.radians(target_position_joint[3]), 'P')
+    robot.motors[9].target = (math.radians(target_position_joint[4]), 'P')
+    robot.IMUBalance(0,0)
+    robot.moveAllToTarget()
