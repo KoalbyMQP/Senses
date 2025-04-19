bf4e55694eaf283a5971ad72f81edccf145b71f0
KoalbyMQP <atphan@wpi.edu>
2025-04-18 20:06:01 -0400

    Update finlyPickAndPlace.py

--- Gripper/pickandplace/finlyPickAndPlace.py
+++ Gripper/pickandplace/finlyPickAndPlace.py
@@ -29,7 +29,6 @@ urdf_path = found_paths[0]
 left_arm_chain = Chain.from_urdf_file(
     urdf_path,
     base_elements=['shoulder1_left', 'shoulder1_left']
-    
 )
 # creating URDF chain for camera
 camera = Chain.from_urdf_file(
@@ -99,8 +98,8 @@ B = np.array([[-final_points[0]], [final_points[1]], [-final_points[2]], [1]])
 C = np.dot(camera_frame_transformation, B)
 
 leftArmTraj = [
-    [[0,0,0], [20,20,20]],#time 
-    [[.50575,  -.006620, .28607],# joint angles
+    [[0,0,0], [20,20,20]],
+    [[.50575,  -.006620, .28607],
    [C[0],  C[1], C[2]] ],
     [[0,0,0], [0,0,0]],
     [[0,0,0], [0,0,0]]
@@ -112,15 +111,15 @@ target_orientation_z=[1, 0, 0]
 Angle =0
 lastAngle=0
 
-#open gripper
+# open gripper
 
-# while time.time() - startTime < 4:
-#         Angle=Angle+.1
-#         robot.motors[27].target = (math.radians(Angle), 'P')
-#         #robot.IMUBalance(0, 0)
-#         robot.moveAllToTarget()
-#         lastAngle=Angle
-# startTime = time.time()
+while time.time() - startTime < 4:
+        Angle=Angle+.1
+        robot.motors[27].target = (math.radians(Angle), 'P')
+        #robot.IMUBalance(0, 0)
+        robot.moveAllToTarget()
+        lastAngle=Angle
+startTime = time.time()
 
 # move gripper
 while time.time() - startTime < 20:
@@ -141,30 +140,30 @@ while time.time() - startTime < 20:
         robot.moveAllToTarget()
 
 # attempt to orientate
-# target_position_2=turnPosition
-# ik_solution = left_arm_chain.inverse_kinematics(target_position_2, target_orientation=target_orientation_z, orientation_mode="Z", initial_position=ik_solution_2 )
-# leftArmTraj = [
-#     [[0,0,0,0,0], [20,20,20,20,20]],
-#     [[motor_angle_task[1],motor_angle_task[2],motor_angle_task[3],motor_angle_task[4],motor_angle_task[5] ],
-#    [ik_solution[1], ik_solution[2], ik_solution[3],ik_solution[4],ik_solution[5]]] ,
-#     [[0,0,0,0,0], [0,0,0,0,0]],
-#     [[0,0,0,0,0], [0,0,0,0,0]]
-# ]
-# lArm_tj_joint = TrajPlannerTime(leftArmTraj[0], leftArmTraj[1], leftArmTraj[2], leftArmTraj[3])
-# startTime = time.time()
-# while time.time() - startTime < 10:
-#         target_position_joint = lArm_tj_joint.getQuinticPositions(time.time() - startTime)
-#         robot.motors[5].target = (target_position_joint[0], 'P')
-#         robot.motors[6].target = (target_position_joint[1], 'P')
-#         robot.motors[7].target = (target_position_joint[2], 'P')
-#         robot.motors[8].target = (target_position_joint[3], 'P')
-#         robot.motors[9].target = (target_position_joint[4], 'P')
-#        # robot.IMUBalance(0, 0)
-#         robot.moveAllToTarget()
-# startTime = time.time()
-# # close gripper
-# while time.time() - startTime < 8:
-#         lastAngle=lastAngle-.1
-#         robot.motors[27].target = (math.radians(lastAngle), 'P')
-#         #robot.IMUBalance(0, 0)
-#         robot.moveAllToTarget()
+target_position_2=turnPosition
+ik_solution = left_arm_chain.inverse_kinematics(target_position_2, target_orientation=target_orientation_z, orientation_mode="Z", initial_position=ik_solution_2 )
+leftArmTraj = [
+    [[0,0,0,0,0], [20,20,20,20,20]],
+    [[motor_angle_task[1],motor_angle_task[2],motor_angle_task[3],motor_angle_task[4],motor_angle_task[5] ],
+   [ik_solution[1], ik_solution[2], ik_solution[3],ik_solution[4],ik_solution[5]]] ,
+    [[0,0,0,0,0], [0,0,0,0,0]],
+    [[0,0,0,0,0], [0,0,0,0,0]]
+]
+lArm_tj_joint = TrajPlannerTime(leftArmTraj[0], leftArmTraj[1], leftArmTraj[2], leftArmTraj[3])
+startTime = time.time()
+while time.time() - startTime < 10:
+        target_position_joint = lArm_tj_joint.getQuinticPositions(time.time() - startTime)
+        robot.motors[5].target = (target_position_joint[0], 'P')
+        robot.motors[6].target = (target_position_joint[1], 'P')
+        robot.motors[7].target = (target_position_joint[2], 'P')
+        robot.motors[8].target = (target_position_joint[3], 'P')
+        robot.motors[9].target = (target_position_joint[4], 'P')
+       # robot.IMUBalance(0, 0)
+        robot.moveAllToTarget()
+startTime = time.time()
+# close gripper
+while time.time() - startTime < 8:
+        lastAngle=lastAngle-.1
+        robot.motors[27].target = (math.radians(lastAngle), 'P')
+        #robot.IMUBalance(0, 0)
+        robot.moveAllToTarget()
