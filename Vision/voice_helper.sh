34c6f7e0c89d20c0c888b459a85d8eb7a355d5c9
macethewindu66 <madduxberry31@gmail.com>
2025-07-01 07:38:31 -0700

    Added a -u flag to the python3 call in voice_helper.sh to eliminate python output buffer for console logging

--- Vision/voice_helper.sh
+++ Vision/voice_helper.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR="/home/finley/Documents/GitHub/Senses/Vision"
 echo "Voice helper started"
 sleep 1
 echo "Processing complete"
-python3 "$SCRIPT_DIR/depthai_demo.py" -cnn yolo-v3-tiny-tf -s color "$@" 
+python3 -u "$SCRIPT_DIR/depthai_demo.py" -cnn yolo-v3-tiny-tf -s color "$@" 
 #python3 depthai_demo.py -cnn yolov8n_coco_640x352 -s color "$@"
